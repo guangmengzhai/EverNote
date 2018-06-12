f33489820e53a1bb9eb5a6b7a5aae650709a2cdc
guangmeng.zhai <guangmeng.zhai@ebaotech.com>
2018-06-12 19:07:28 +0800

    what:sql script tips

---
+++ docs/SqlScriptTips.sql
@@ -0,0 +1,7 @@
+
+/** 提取 中文‘Name’中的首字母 **/
+update t_gb_district set alphabet =ELT(INTERVAL(CONV(HEX(left(CONVERT(t_gb_district.NAME USING gbk),1)),16,10),   
+          0xB0A1,0xB0C5,0xB2C1,0xB4EE,0xB6EA,0xB7A2,0xB8C1,0xB9FE,0xBBF7,   
+          0xBFA6,0xC0AC,0xC2E8,0xC4C3,0xC5B6,0xC5BE,0xC6DA,0xC8BB,  
+          0xC8F6,0xCBFA,0xCDDA,0xCEF4,0xD1B9,0xD4D1),   
+      'A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','W','X','Y','Z');
